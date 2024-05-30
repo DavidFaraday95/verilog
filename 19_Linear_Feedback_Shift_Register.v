989e795c04a3315d8729f54685b23cfc114f3e65
DavidFaraday95 <46838417+DavidFaraday95@users.noreply.github.com>
2024-05-30 23:29:15 +0200

    Create 19_Linear_Feedback_Shift_Register.v

---
+++ 19_Linear_Feedback_Shift_Register.v
@@ -0,0 +1,26 @@
+`ifndef LFSR_V
+`define LFSR_V
+
+module LFSR (clk, reset, enable, lfsr);
+
+  parameter TAPS = 8'b11101;  // bitmask for taps
+  parameter INVERT = 0;       // invert feedback bit?
+  localparam NBITS = $size(TAPS);      // bitwidth
+
+  input clk, reset;
+  input enable;
+  output reg [NBITS-1:0] lfsr;         // shift register
+
+  wire feedback = lfsr[NBITS-1] ^ INVERT;
+
+  always @(posedge clk)
+    begin
+      if (reset)
+        lfsr <= {lfsr[NBITS-2:0], 1'b1};   // reset loads with all 1s
+      else if (enable)
+        lfsr <= {lfsr[NBITS-2:0], 1'b0} ^ (feedback ? TAPS : 0);
+    end
+
+endmodule
+
+`endif
